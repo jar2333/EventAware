5f78ba28d0c1a7297457c0d30772f80dca26399e
jose <jar2333@columbia.edu>
2022-10-30 23:07:23 +0000

    authentication user story

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,3 +1,10 @@
-Given /I am logged in as "(.*)"/ do |uni|
+Given /I am( not)? (logged in|authenticated) as "(.*)"/ do |logged, uni|
     
+end
+
+Then /I am in the (login|authentication) page/ do 
+end
+
+Given /I submit (in)?valid credentials for user "(.*)"/ do |valid, user|
+
 end
