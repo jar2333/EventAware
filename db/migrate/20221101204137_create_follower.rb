5e27268d64c995dbc8d8d0664775007be3170828
Aarushi Jain <aj3087@columbia.edu>
2022-11-01 20:54:49 +0000

    changes rolled back

--- db/migrate/20221101204137_create_follower.rb
+++ db/migrate/20221101204137_create_follower.rb
@@ -1,8 +1,8 @@
 class CreateFollower < ActiveRecord::Migration[5.2]
   def change
     create_table :followers do |t|
-      t.id :user_id
-      t.id :follower_id
+      t.string :user_id
+      t.string :follower_id
     end
   end
 end
--- db/migrate/20221101204252_create_authentication.rb
+++ db/migrate/20221101204252_create_authentication.rb
@@ -1,7 +1,7 @@
 class CreateAuthentication < ActiveRecord::Migration[5.2]
   def change
     create_table :authentications do |t|
-      t.id :user_id
+      t.string :user_id
       t.string :auth_hash
       t.string :auth_token
     end
