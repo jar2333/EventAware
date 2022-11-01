962c1d6cb6afda97fc7fcf1efe3203a1f7687fe5
Aarushi Jain <aj3087@columbia.edu>
2022-11-01 20:43:21 +0000

    Modified id

--- db/migrate/20221101204137_create_follower.rb
+++ db/migrate/20221101204137_create_follower.rb
@@ -1,8 +1,8 @@
 class CreateFollower < ActiveRecord::Migration[5.2]
   def change
     create_table :followers do |t|
-      t.string :user_id
-      t.string :follower_id
+      t.id :user_id
+      t.id :follower_id
     end
   end
 end
