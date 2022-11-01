3bd41ffa3c72fd29d989686b7fd49129613eb4b1
jose <jar2333@columbia.edu>
2022-11-01 23:10:51 +0000

    create event feature

--- features/step_definitions/create_event_steps.rb
+++ features/step_definitions/create_event_steps.rb
@@ -1,2 +1,9 @@
+Given /I (am|should be) on a create event page/ do
+
+end
+
 Given /I (press|click) the create event button/ do 
+end
+
+Then /I enter "(*.)" in the (*.) field/ do |text, field|
 end
