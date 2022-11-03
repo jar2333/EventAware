646bf6b358078bfc4bc063b4b4e28c2d81e9e42c
jose <jar2333@columbia.edu>
2022-11-03 01:18:39 +0000

    all main pages except new/edit event are up

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,10 +8,24 @@ class EventsController < ApplicationController
   end
 
   def info
-    
+    @user = params[:uni]
+    #if logged in as this user, also render edit and delete buttons
+    @authenticated = false
+
+    @id = params[:id]
+    @event_info = Event.find(@id)
+
+    render 'frontend/event_info'
   end
 
   def chat
+    @user = params[:uni]
+
+    @id = params[:id]
+
+    @chat = Message.where(event_id: @id)
+
+    render 'frontend/chat'
   end
 
   def index
